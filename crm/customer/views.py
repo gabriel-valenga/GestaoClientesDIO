fce52e1b3c84e76f6d873d8347841c5e9f969127
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2021-12-02 11:57:30 -0300

    Criados update e delete dos clientes; Criada paginação dos clientes da lista

--- crm/customer/views.py
+++ crm/customer/views.py
@@ -1,11 +1,13 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Customer
 from .forms import CustomerForm
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
 
 class CustomerListView(ListView):
     template_name = 'customer/customer_list.html'
+    paginate_by = 5
     model = Customer
     queryset = Customer.objects.all()
 
@@ -20,3 +22,26 @@ class CustomerCreateView(CreateView):
     def get_success_url(self):
         return reverse('customer:customer-list')
 
+
+class CustomerUpdateView(UpdateView):
+    template_name = 'customer/customer.html'
+    form_class = CustomerForm
+
+    def get_object(self):
+        customer_id = self.kwargs.get('id')
+        return get_object_or_404(Customer, id=customer_id)
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('customer:customer-list')
+
+
+class CustomerDeleteView(DeleteView):
+    def get_object(self):
+        customer_id = self.kwargs.get('id')
+        return get_object_or_404(Customer, id=customer_id)
+
+    def get_success_url(self):
+        return reverse('customer:customer-list')
