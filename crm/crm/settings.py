d8298064ee89bedfdbef5ee4ac8eaaa0707c4dcc
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2021-12-06 16:54:49 -0300

    Criada funcionalidade de busca na tela de clientes, e adicionado template da tela de login

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from .secret import DJANGO_SECRET_KEY
+from .secret import DJANGO_SECRET_KEY, CORE_TEMPLATE_DIR
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'crm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [CORE_TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
