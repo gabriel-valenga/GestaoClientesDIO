d48a84441c55ab737d50abdcd7382a240d5b9028
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2021-12-17 11:40:51 -0300

    Removida subpasta crm, movidas pastas dos apps diretamnte para a raiz do projeto

--- crm/settings.py
+++ crm/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'crm.customer',
-    'crm.core'
+    'customer',
+    'core'
 ]
 
 MIDDLEWARE = [
